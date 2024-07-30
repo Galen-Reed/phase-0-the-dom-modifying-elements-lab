c0c37a0ac6def467d0605b61632d7cad46f96b2c
GalenReed <galenreed@vt.edu>
2024-07-30 17:53:52 -0400

    galen is victorious

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+newHeader.textContent = "Galen is the champion";
+newHeader.id = "victory";
+document.head.append(newHeader);
